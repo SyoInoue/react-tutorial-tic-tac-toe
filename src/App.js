277a92f4c95d505aabf0e8fd931c63d4403d0c90
SyoInoue <funnyfacee7777@gmail.com>
2023-08-17 13:54:47 +0900

    マス目を全部ハードコードするのではなく、Board を 2 つのループを使ってレンダーするよう書き直す。

--- src/App.js
+++ src/App.js
@@ -34,21 +34,20 @@ function Board({ xIsNext, squares, onPlay }) {
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {Array.from({ length: 3 }).map((_, rowIndex) => (
+        <div key={rowIndex} className="board-row">
+          {Array.from({ length: 3 }).map((_, colIndex) => {
+            const index = rowIndex * 3 + colIndex;
+            return (
+              <Square
+                key={index}
+                value={squares[index]}
+                onSquareClick={() => handleClick(index)}
+              />
+            );
+          })}
+        </div>
+      ))}
     </>
   );
 }
@@ -71,7 +70,6 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
-    let dom;
     if (move > 0) {
       if (currentMove === move) {
         description = <div>{"You are at move #" + move}</div>;
